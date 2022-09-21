cd4434da4f0f040240e2104a3ddf628060815c52
Pavel Filatov <qteeeer@icloud.com>
2022-09-21 20:25:38 +0300

    готово

---
+++ Zada4a_10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10: Напишите программу, которая принимает 
+// на вход трёхзначное число и на выходе показывает 
+// вторую цифру этого числа.
+
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int FindNewDigit(int num)
+{
+    int result = num % 100 / 10;
+    return result;
+}
+int res = FindNewDigit(number);
+Console.WriteLine(res);
---
+++ Zada4a_15/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 15: Напишите программу, которая принимает 
+// на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите число дня недели");
+int day = Convert.ToInt32(Console.ReadLine());
+
+int DayNumber(int day)
+{
+
+    if (day == 6 && day == 7)
+    {
+        Console.WriteLine("День является выходным");
+    }
+    else if (day >= 1 && day <= 5)
+    {
+        Console.WriteLine("День является рабочим");
+    }
+    else 
+    {
+            Console.WriteLine("В неделе 7 дней, введите от 1 до 7"); 
+    }
+    return  day;
+}
+DayNumber(day);
